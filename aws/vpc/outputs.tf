77da220324365e4b93c630752a31b189cfe0d367
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-09-11 17:27:48 +0200

    adding VPC module

---
+++ aws/vpc/outputs.tf
@@ -0,0 +1,30 @@
+#
+# VPC outputs
+#
+output "vpc_id" {
+  description = "The VPC ID for the VPC"
+  value       = aws_vpc.infra.id
+}
+
+output "vpc_cidr" {
+  description = "The CIDR for the VPC"
+  value       = var.vpc_cidr
+}
+
+output "vpc_public_subnet" {
+  description = "The public subnet for the VPC"
+  value       = var.vpc_public_subnet
+}
+
+output "vpc_private_subnet" {
+  description = "The private subnet for the VPC"
+  value       = var.vpc_private_subnet
+}
+
+#
+# Bastion outputs
+#
+output "bastion_ip" {
+  description = "The EIP attached to the bastion EC2 server"
+  value       = var.bastion_instance ? aws_instance.bastion[0].public_ip : "Bastion not deployed"
+}
---
+++ aws/vpc/variables.tf
@@ -0,0 +1,58 @@
+# Cycloid
+variable "customer" {}
+variable "env" {}
+variable "project" {}
+
+# AWS
+variable "keypair_public" {
+  description = "The public SSH key, for SSH access to newly-created instances"
+}
+
+#
+# VPC
+#
+variable "vpc_cidr" {
+  type        = string
+  description = "The CIDR of the VPC."
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_public_subnet" {
+  type        = string
+  description = "The public subnet for the VPC."
+  default     = "10.0.0.0/24"
+}
+
+variable "vpc_private_subnet" {
+  type        = string
+  description = "The private subnet for the VPC."
+  default     = "10.0.1.0/24"
+}
+
+#
+# Bastion
+#
+variable "bastion_instance" {
+  description = "Whether to deploy a bastion instance or not"
+  default     = false
+}
+
+variable "bastion_instance_type" {
+  description = "Instance type for the bastion"
+  default     = "t3.micro"
+}
+
+# Tags
+variable "extra_tags" {
+  default = {}
+}
+
+locals {
+  standard_tags = {
+    "cycloid.io" = "true"
+    env          = var.env
+    project      = var.project
+    customer     = var.customer
+  }
+  merged_tags = merge(local.standard_tags, var.extra_tags)
+}

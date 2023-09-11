b0b01b66dbd7e32b1ee05cde8df89b2af1e2619d
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-09-11 13:08:00 +0200

    adding RDS module

---
+++ aws/rds/network.tf
@@ -0,0 +1,13 @@
+data "aws_subnets" "private" {
+  filter {
+    name = "vpc-id"
+    values = [
+      var.vpc_id
+    ]
+  }
+
+  filter {
+    name = "map-public-ip-on-launch"
+    values = [false]
+  }
+}

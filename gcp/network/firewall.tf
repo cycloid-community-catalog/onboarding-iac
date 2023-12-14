5d26641a86f2bfd5019a2bec0253f4154735eaa5
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-12-14 16:04:33 +0100

    minor update

---
+++ gcp/network/firewall.tf
@@ -0,0 +1,59 @@
+resource "google_compute_firewall" "allow-internal" {
+  name    = "${var.customer}-${var.project}-${var.env}-allow-internal"
+  network = google_compute_network.vpc.id
+
+  allow {
+    protocol = "icmp"
+  }
+  
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+  
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+  
+  source_ranges = [
+    var.public_subnet_cidr,
+    var.private_subnet_cidr
+  ]
+}
+
+resource "google_compute_firewall" "allow-http" {
+  name    = "${var.customer}-${var.project}-${var.env}-allow-http"
+  network = google_compute_network.vpc.id
+  
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  target_tags = ["http"] 
+}
+
+resource "google_compute_firewall" "allow-https" {
+  name    = "${var.customer}-${var.project}-${var.env}-allow-https"
+  network = google_compute_network.vpc.id
+  
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  target_tags = ["https"] 
+}
+
+resource "google_compute_firewall" "allow-bastion" {
+  name    = "${var.customer}-${var.project}-${var.env}-allow-bastion"
+  network = google_compute_network.vpc.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags = ["ssh"]
+}
--- gcp/network/network.tf
+++ gcp/network/network.tf
@@ -1,4 +1,4 @@
-resource "google_compute_network" "compute_network" {
+resource "google_compute_network" "vpc" {
   name                    = "${var.customer}-${var.project}-${var.env}"
   auto_create_subnetworks = false
   routing_mode            = "REGIONAL"
@@ -7,27 +7,26 @@ resource "google_compute_network" "compute_network" {
 resource "google_compute_subnetwork" "compute_public_subnetwork" {
   name                     = "${var.customer}-${var.project}-${var.env}-private"
   ip_cidr_range            = var.public_subnet_cidr
-  network                  = google_compute_network.compute_network.id
+  network                  = google_compute_network.vpc.id
   private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "compute_private_subnetwork" {
   name                     = "${var.customer}-${var.project}-${var.env}-private"
   ip_cidr_range            = var.private_subnet_cidr
-  network                  = google_compute_network.compute_network.id
+  network                  = google_compute_network.vpc.id
   private_ip_google_access = true
 }
 
 # NAT ROUTER
 resource "google_compute_router" "compute_router" {
   name    = "${var.customer}-${var.project}-${var.env}"
-  network = google_compute_network.compute_network.id
+  network = google_compute_network.vpc.id
 }
 
 resource "google_compute_router_nat" "compute_router_nat" {
   name                               = "${var.customer}-${var.project}-${var.env}"
-  router                             = google_compute_router.this.name
-  region                             = google_compute_router.this.region
+  router                             = google_compute_router.compute_router.name
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
   subnetwork {

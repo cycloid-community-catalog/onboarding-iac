ce31fc2d72a0d38dc81e356d6999102f9c2f28cf
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2024-01-02 17:27:37 +0100

    minor fix

--- gcp/network/bastion.tf
+++ gcp/network/bastion.tf
@@ -1,6 +1,6 @@
 module "iap_bastion" {
   source = "terraform-google-modules/bastion-host/google"
-  version = "2.9.0"
+  version = "6.0.0"
 
   network = google_compute_network.vpc.id
   subnet = google_compute_subnetwork.compute_public_subnetwork.id

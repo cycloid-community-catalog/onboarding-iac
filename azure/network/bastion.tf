bec449b62402346f8de99cdeb1f073c0db01c4f5
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2024-01-05 22:40:20 +0100

    minor fix

--- azure/network/bastion.tf
+++ azure/network/bastion.tf
@@ -1,27 +1,27 @@
-resource "azurerm_public_ip" "public_ip" {
-  name                = "${var.customer}-${var.project}-${var.env}"
-  location            = var.azure_location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+# resource "azurerm_public_ip" "public_ip" {
+#   name                = "${var.customer}-${var.project}-${var.env}"
+#   location            = var.azure_location
+#   resource_group_name = var.resource_group_name
+#   allocation_method   = "Static"
+#   sku                 = "Standard"
 
-  depends_on = [
-    azurerm_resource_group.resource_group
-  ]
-}
+#   depends_on = [
+#     azurerm_resource_group.resource_group
+#   ]
+# }
 
-resource "azurerm_bastion_host" "bastion_host" {
-  name                = "${var.customer}-${var.project}-${var.env}"
-  location            = var.azure_location
-  resource_group_name = var.resource_group_name
+# resource "azurerm_bastion_host" "bastion_host" {
+#   name                = "${var.customer}-${var.project}-${var.env}"
+#   location            = var.azure_location
+#   resource_group_name = var.resource_group_name
 
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.public_subnet.id
-    public_ip_address_id = azurerm_public_ip.public_ip.id
-  }
+#   ip_configuration {
+#     name                 = "configuration"
+#     subnet_id            = azurerm_subnet.public_subnet.id
+#     public_ip_address_id = azurerm_public_ip.public_ip.id
+#   }
 
-  depends_on = [
-    azurerm_resource_group.resource_group
-  ]
-}
+#   depends_on = [
+#     azurerm_resource_group.resource_group
+#   ]
+# }
